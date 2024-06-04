7fd168dc5fc1ac63f19689588c17978c6da6675c
Matvey <36668268+slmatthew@users.noreply.github.com>
2024-06-04 22:19:23 +0300

    feat(wpf): add WPF project

---
+++ PinkClient/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PinkClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
