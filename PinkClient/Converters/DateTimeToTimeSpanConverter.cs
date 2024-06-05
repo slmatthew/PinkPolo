deed5c3eca980b7ba7744e00b7291e312e4fdff2
Matvey <36668268+slmatthew@users.noreply.github.com>
2024-06-06 01:44:49 +0300

    feat(wpf): create datetime converters

---
+++ PinkClient/Converters/DateTimeToTimeSpanConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace PinkClient.Converters
+{
+    public class DateTimeToTimeSpanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+            {
+                return dateTime.TimeOfDay;
+            }
+            return TimeSpan.Zero;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TimeSpan timeSpan)
+            {
+                return DateTime.Today.Add(timeSpan);
+            }
+            return DateTime.Now;
+        }
+    }
+}
